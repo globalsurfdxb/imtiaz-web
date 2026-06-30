372ff614e3c18f8786a18eb5be768b7cf49184ac
Hisham <hisham@globalsurf.ae>
2026-06-30 11:51:44 +0530

    fix: snap-scroll in mobile

--- app/components/Home/sections/SpotlightSliderSnap.tsx
+++ app/components/Home/sections/SpotlightSliderSnap.tsx
@@ -358,7 +358,7 @@ export default function SustainabilitySpotlight({
               slides={slides}
               targetRef={mImgsRef}
               parallaxRef={mParallaxRef}
-              className="w-full relative h-[308px] sm:h-[400px] overflow-hidden cursor-grab active:cursor-grabbing select-none mb-5 sm:mb-50 border border-red-500"
+              className="w-full relative h-[308px] sm:h-[400px] overflow-hidden cursor-grab active:cursor-grabbing select-none mb-5 sm:mb-50"
               pointerHandlers={pointerHandlers}
             />
           </div>
